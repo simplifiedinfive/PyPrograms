cab8fd3a28cc739c837b63eec582b89ec0fe63af
simplifiedinfive <42615618+simplifiedinfive@users.noreply.github.com>
2018-11-17 22:30:21 +0530

    Update dataclasses_py.py

--- dataclasses_py.py
+++ dataclasses_py.py
@@ -1,1 +1,46 @@
+from dataclasses import dataclass
 
+class OldStyleStudentInfo:
+    roll_no = 0
+    name = ''
+    subjects = []
+
+    def __init__(self, roll_no, name, subjects):
+        self.roll_no = roll_no
+        self.name = name
+        self.subjects = subjects
+
+@dataclass(order=True, unsafe_hash=True)
+class StudentInfo:
+    roll_no: int
+    name: str
+    subjects: list
+
+std1 = StudentInfo(14,'Vijay',['Maths','Science'])
+std2 = StudentInfo(15,'Ajay',['Hindi','English'])
+
+if std1 != std2:
+    print('2 students are not equal')
+
+#data classes are mutable by default
+std1.roll_no=15
+std1.name='Ajay'
+std1.subjects = ['Hindi', 'English']
+
+if std1 == std2:
+    print('2 students are equal')
+
+#use frozen parameter to make an immutable data class
+@dataclass(frozen=True)
+class Marks:
+    maths: int
+    english: int
+    science: int
+    hindi: int
+
+
+m_vijay = Marks(30,40,50,60)
+#throws dataclasses.FrozenInstanceError: cannot assign to field 'english'
+#m_vijay.english=55
+
+print(m_vijay)
